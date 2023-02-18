b4ee9b5973c16baf6c0d4e97189116ea5e11ebd3
fen <zhifenli1121@gmail.com>
2023-02-17 21:58:33 -0500

    add comment

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+/*********************************************************************************
+ *  WEB322 – Lab 3
+ *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+ *  No part of this assignment has been copied manually or electronically from any other source
+ *  (including web sites) or distributed to other students.
+ *
+ *  Name: __Zhifen Li____ Student ID: 168833218___ Date: __2013-02-17_____
+ *
+ *  Cyclic Web App URL: _________https://energetic-seal-tutu.cyclic.app/________
+ *
+ *  GitHub Repository URL: ____https://github.com/zhifenli/lab3______________
+ *
+ ********************************************************************************/
 const HTTP_PORT = process.env.PORT || 8080;
 const express = require("express");
 const path = require("path");
