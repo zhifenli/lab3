dd6bf675bd825165dc2e22f8b38ff96bd0157c55
fen <zhifenli1121@gmail.com>
2023-02-17 21:55:57 -0500

    update

--- index.js
+++ index.js
@@ -36,15 +36,6 @@ app.engine(
 app.set("view engine", ".hbs");
 
 app.get("/", (req, res) => {
-  console.log("Hello");
-  res.send("Hello");
-});
-
-app.get("/index", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/index.js"));
-});
-
-app.get("/viewData", (req, res) => {
   res.render("viewData", {
     data: data,
     layout: false,
